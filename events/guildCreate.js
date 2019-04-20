1995d117d68d479ec278d2bd8b9489e23670bc1f
Jakob Mellberg <jakob@Jakobs-MacBook-Pro.local>
2019-04-20 20:49:48 +0200

    Message the owner when the bot joins

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,3 +1,12 @@
 module.exports = (client, guild) => {
   console.log("Joined a new guild: " + guild.name);
+
+  //console.log(guild.owner.user.username);
+  guild.owner.send(
+    "Hello " +
+      guild.owner.user.username +
+      ", owner of " +
+      guild.name +
+      ".\nPlease create a textchannel named moverbot and write !help to start using Moverbot!\nAutomation of this process is being developed."
+  );
 };
