c74fe3063f4710724df52271349cb345b991101f
Qwiko <jakob.mellberg@gmail.com>
2020-01-03 13:47:47 +0100

    Simplified clear command

--- commands/clear.js
+++ commands/clear.js
@@ -8,38 +8,17 @@ exports.run = async function(client, message) {
     );
     return;
   }
-  while (true) {
-    messages = await message.channel.fetchMessages({
-      limit: 100
-    });
-    if (messages.size == 0) break;
-    await message.channel
-      .bulkDelete(messages, true)
-      .then(deletedMessages => {
-        //Check if filter is needed or if bulkdelete deleted all messages.
-        if (messages.size != deletedMessages.size) {
-          //Filtering out messages that did not get deleted.
-          messages = messages.filter(val => {
-            !deletedMessages.array().includes(val);
-          });
-          if (messages.size > 0) {
-            //Deleting messages older than 14 days
-            messages.deleteAll();
-          }
-        }
-      })
-      //}
-      .catch(function(error) {
-        if (error.code == 50013) {
-          console.log(error.code);
-        } else if (error.code == 10008) {
-          //Unknown Message
-          console.log(error.code);
-        } else {
-          console.log(error);
-        }
-      });
-  }
+   
+  messages = await message.channel.fetchMessages({ limit: 100 });
+  //No messages in the channel, do not have to delete anything
+
+  await message.channel.bulkDelete(messages, true);
+  
+  // Get messages
+  message.channel.fetchMessages({ limit: 100 })
+  .then(messages => messages.deleteAll())
+  .catch(console.error);
+
   //After cleared print out the helpmessage again.
   helpMessage(client, message);
 };
