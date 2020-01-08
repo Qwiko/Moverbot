6456701fbec7ec1d78b77311cc9ab951d078e69a
Qwiko <jakob.mellberg@gmail.com>
2020-01-08 19:28:04 +0100

    await fix

--- commands/clear.js
+++ commands/clear.js
@@ -1,5 +1,3 @@
-const helpMessage = require("../lib/helpMessage.js");
-
 exports.run = async function(client, message) {
   //Check if administrator
   if (!message.member.hasPermission("ADMINISTRATOR")) {
@@ -8,16 +6,25 @@ exports.run = async function(client, message) {
     );
     return;
   }
-   
+
   messages = await message.channel.fetchMessages({ limit: 100 });
   //No messages in the channel, do not have to delete anything
 
-  await message.channel.bulkDelete(messages, true);
-  
+  await message.channel.bulkDelete(messages, true).catch(function(error) {
+    /* Handle error here for browser new page and return
+       expected value for page if things fail */
+    console.log("bulk");
+    console.log(error);
+  });
+
   // Get messages
-  message.channel.fetchMessages({ limit: 100 })
-  .then(messages => messages.deleteAll())
-  .catch(console.error);
+  await message.channel
+    .fetchMessages({ limit: 100 })
+    .then(messages => messages.deleteAll())
+    .catch(error => {
+      console.log("single");
+      console.log(error);
+    });
 
   //After cleared print out the helpmessage again.
   //helpMessage(client, message);
