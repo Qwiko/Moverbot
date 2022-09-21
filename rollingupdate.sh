8ced842a9beec89474d1a29091f15e6806ce4855
Jakob Mellberg <jakob.mellberg@gmail.com>
2022-09-21 17:34:53 +0200

    Changed to word count instead of line count

--- rollingupdate.sh
+++ rollingupdate.sh
@@ -2,7 +2,7 @@ recreate_service() {
 
     echo "Rolling update for $1"
     PREVIOUS_CONTAINER=$(docker ps --format "table {{.ID}}  {{.Names}}  {{.CreatedAt}}" | grep $1 | awk -F  "  " '{print $1}')
-    CURRENT_COUNT=$(echo $PREVIOUS_CONTAINER | wc -l)
+    CURRENT_COUNT=$(echo $PREVIOUS_CONTAINER | wc -w)
     echo "Current count: $CURRENT_COUNT"
     docker compose up -d --no-deps --scale $1=$(($CURRENT_COUNT+1)) --no-recreate $1
     sleep 10
