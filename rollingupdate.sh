bc95af545b8d2fd4688d95c62902952a4bb675f9
Jakob Mellberg <jakob.mellberg@gmail.com>
2022-09-21 17:36:56 +0200

    Fixes to rollingupdate script

--- rollingupdate.sh
+++ rollingupdate.sh
@@ -3,10 +3,10 @@ recreate_service() {
     echo "Rolling update for $1"
     PREVIOUS_CONTAINER=$(docker ps --format "table {{.ID}}  {{.Names}}  {{.CreatedAt}}" | grep $1 | awk -F  "  " '{print $1}')
     CURRENT_COUNT=$(echo $PREVIOUS_CONTAINER | wc -w)
-    echo "Current count: $CURRENT_COUNT"
-    docker compose up -d --no-deps --scale $1=$(($CURRENT_COUNT+1)) --no-recreate $1
+    echo "Current count: $CURRENT_COUNT, adding $CURRENT_COUNT more"
+    docker compose up -d --no-deps --scale $1=$(($CURRENT_COUNT+$CURRENT_COUNT)) --no-recreate $1
     sleep 10
-    echo "Removing the old services"
+    echo "Removing the old containers"
     docker kill -s SIGTERM $PREVIOUS_CONTAINER
     sleep 1
     docker rm -f $PREVIOUS_CONTAINER
