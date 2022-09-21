409e8ac7aa7a4345d72c70bdfed04e2de151520c
Jakob Mellberg <jakob.mellberg@gmail.com>
2022-09-21 17:16:36 +0200

    fmt change

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -108,7 +108,7 @@ func main() {
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	fmt.Println("Bot is now running")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
