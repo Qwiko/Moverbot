1ad9c63c072268af5e77b6c35737a7e33be523fb
Jakob Mellberg <jakob.mellberg@gmail.com>
2022-09-21 17:23:38 +0200

    Added rollingupdate docker compose script

---
+++ rollingupdate.sh
@@ -0,0 +1,14 @@
+recreate_service() {
+
+    echo "Rolling update for $1"
+    PREVIOUS_CONTAINER=$(docker ps --format "table {{.ID}}  {{.Names}}  {{.CreatedAt}}" | grep $1 | awk -F  "  " '{print $1}')
+    docker-compose up -d --no-deps --scale $1=3 --no-recreate $1
+    sleep 100
+    docker kill -s SIGTERM $PREVIOUS_CONTAINER
+    sleep 1
+    docker rm -f $PREVIOUS_CONTAINER
+    docker-compose up -d --no-deps --scale $1=2 --no-recreate $1
+}
+
+recreate_service "worker"
+recreate_service "cache"
